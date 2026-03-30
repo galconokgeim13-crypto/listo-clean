122074324e9f98b125a0f948ee0336f275934ecf
galconokgeim13-crypto <galconokgeim13@gmail.com>
2026-03-30 19:50:54 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,4 @@
 import React, { useState } from 'react';
-import { SafeAreaView, <SafeAreaView style={{ flex: 1, padding: 20 }}> Text, TextInput, Button, FlatList } from 'react-native';
-
-export default function App() {
-  const [products, setProducts] = useState([]);
-  const [text, setText] = useState('');
-
-  const addProduct = () => {
-  if (!text.trim()) return;
-
-  setProducts(prev => [
-    { id: Date.now().toString(), name: text.trim()},
-    ...prev
-  ]);
-
-  setText('');
-};
-
-  return (
- import React, { useState } from 'react';
 import { SafeAreaView, View, Text, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
@@ -37,8 +18,10 @@ export default function App() {
 
   return (
     <SafeAreaView style={{ flex: 1, padding: 20 }}>
-      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Список продуктов</Text>
-      
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
+        Список продуктов
+      </Text>
+
       <View style={{ flexDirection: 'row', marginTop: 20, marginBottom: 20 }}>
         <TextInput
           style={{ flex: 1, borderWidth: 1, padding: 10, marginRight: 10 }}
@@ -48,23 +31,16 @@ export default function App() {
         />
         <Button title="Добавить" onPress={addProduct} />
       </View>
-      
+
       <FlatList
         data={products}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
-          <Text style={{ fontSize: 18, marginTop: 5 }}>• {item.name}</Text>
+          <Text style={{ fontSize: 18, marginTop: 5 }}>
+            • {item.name}
+          </Text>
         )}
       />
     </SafeAreaView>
-  );
-}
-      
-      <FlatList
-        data={products}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => <Text style={{ fontSize: 18, marginTop: 5 }}>• {item.name}</Text>}
-      />
-    </View>
   );
 }
